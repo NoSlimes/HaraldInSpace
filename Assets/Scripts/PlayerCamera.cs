c46d4ddb7a6e9da5a9ac6a33908880dd0c05fa7e
Elias Wennerberg <elias.wennerberg@learnet.se>
2022-10-19 12:18:33 +0200

    perfekt scen

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -6,14 +6,11 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
 
-    public float sensX;
-    public float sensY;
+    public float mouseSensitivity = 100f; // skapar ett nummer som blir mouseSensitivity, går att ändra på tackvare public. 
 
-    public Transform orientation;
+    public Transform playerBody; // kallar på spelarens vektorer och nämner den playerBody
 
-    float xRotation;
-    float yRotation; 
-
+    float xRotation = 0f;  // skapar ett nummer med namnet xRotation. 
     // Start is called before the first frame update
     void Start()
     {
@@ -26,17 +23,16 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; // kallar på mouseX inputen och gångar det med mouseSensitivity och Time.deltatime
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; // samma som mouseX fast med Y axis. 
 
-        yRotation += mouseX;
 
-        xRotation -= mouseY; 
+        xRotation -= mouseY; // xRotationen är lika med -mouseY, Subtraherar resultatet genom att använda -= 
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clampar xRotationen så att kameran inte går full exorcist mode (vrider sig 360 grader överallt) 
 
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
 
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Roterar kameran genom Y-axeln 
+        playerBody.Rotate(Vector3.up * mouseX); // Roterar spelarens kropp 
 
     }
 }

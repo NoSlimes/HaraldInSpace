0e8f41005c5807ef5bedb3dd34fab16d52b409d9
Bigblashbanana <75020639+Bigblashbanana@users.noreply.github.com>
2022-10-19 13:43:37 +0200

    jumpingworkssomewhatlol

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,58 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-public float movementSpeed = 10f;
-public float lookatspeed = 5f;
+
+    public float movementSpeed = 1000;
+    public float jumpStrength = 100000;
 
 
-    void Update () {
- 
-        if (Input.GetKey ("w")) {
-            transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * movementSpeed;
-        }   else if (Input.GetKey ("s")) {
-            transform.position -= transform.TransformDirection (Vector3.forward) * Time.deltaTime * movementSpeed;
+    public Rigidbody playerBody;
+   
+
+    bool isGrounded; 
+    
+    void Start()
+    {
+
+
+
+    }
+
+    void Update()
+    {
+         
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            playerBody.AddRelativeForce(Vector3.forward * movementSpeed * Time.deltaTime);
+
         }
- 
-        if (Input.GetKey ("a") && !Input.GetKey ("d")) {
-                transform.position += transform.TransformDirection (Vector3.left) * Time.deltaTime * movementSpeed;
-            } else if (Input.GetKey ("d") && !Input.GetKey ("a")) {
-                transform.position -= transform.TransformDirection (Vector3.left) * Time.deltaTime * movementSpeed;
-            }
-
-        //mouse look at
-        float horizontal = Input.GetAxis("Mouse X") * lookatspeed;
-		float vertical = Input.GetAxis("Mouse Y") * lookatspeed;
-
-		transform.Rotate(0f, horizontal, 0f, Space.World);
-		//transform.Rotate(-vertical, 0f, 0f, Space.Self);
+        else if (Input.GetKey(KeyCode.S))
+        {
+            playerBody.AddRelativeForce(Vector3.forward * -movementSpeed * Time.deltaTime);
         }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            playerBody.AddRelativeForce(Vector3.left * movementSpeed * Time.deltaTime);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerBody.AddRelativeForce(Vector3.left * -movementSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerBody.AddRelativeForce(Vector3.up * jumpStrength * Time.deltaTime); 
+        }
+        
+
+    }
+    
+
+
 }
